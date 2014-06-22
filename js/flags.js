1d9f1eaecb9ce8d6bc26f43898a5428d3beb2c60
jin ruan <jruan6377@bths.edu>
2014-06-22 00:02:01 -0400

    Added flags

--- js/flags.js
+++ js/flags.js
@@ -1,22 +1,32 @@
 /**
  * Created by minerva on 6/20/14.
  */
-/*
-var images = document.getElementsByTagName('img');
-var srcList = [];
+function kimonoCallback(data) {
+    // do something with the data
+    // please make sure the scope of this function is global
+    $.each(data.results.collection2, function(key,value)
+        {
+            console.log(value.property2.src);
+            //$("#team1flag").append("<img src= " + " ' " + value.property2.src + " ' " + "/>");
+            //$("#team2flag").append("<img src= " + " ' " + value.property2.src + " ' " + "/>");
 
+            //  $("#team2flag").append("<p>" + value.property2.alt + "</p>");
+          //  $("#flags").append("<option value='" + value.property2.alt + "'>" + "<img src= " + " ' " + value.property2.src + " ' " + "/></option>");
 
-for(var i in images) {
+            var selects = document.getElementById("team1");
+            var selectedValue = selects.options[selects.selectedIndex].value;// will gives u 2
+            if (selectedValue==16){
+                $("#team1flag").append("<img src= " + " ' " + value.property2.src[16] + " ' " + "/>");
+                alert('aloha');
 
-    srcList.push('<a href="' + images[i].src + '"><img src="' + images[i].src + '">');
-    alert(srcList);
+            }
+
+        }
+    );
 }
 
-console.log(images[0]);
-console.log('"></a>');
-*/
-var images = document.getElementsByTagName('img');
-var srcList = [];
-for(var i = 0; i < images.length; i++) {
-    srcList.push(images[i].src);
-}
+$.ajax({
+    "url":"http://www.kimonolabs.com/api/c30l5e5u?apikey=5eadc80e616c893a6320ae4fe3763624&kimlimit=1000&callback=kimonoCallback",
+    "crossDomain":true,
+    "dataType":"jsonp"
+});
