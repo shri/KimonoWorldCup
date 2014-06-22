6702598ad454d13229f6ea0d40ccfc2467d7f79d
jin ruan <jruan6377@bths.edu>
2014-06-22 14:23:18 -0400

    Added flags

--- js/flags.js
+++ js/flags.js
@@ -5,24 +5,32 @@ function kimonoCallback(data) {
     // do something with the data
     // please make sure the scope of this function is global
     $.each(data.results.collection2, function(key,value)
+
         {
-            console.log(value.property2.src);
-            //$("#team1flag").append("<img src= " + " ' " + value.property2.src + " ' " + "/>");
-            //$("#team2flag").append("<img src= " + " ' " + value.property2.src + " ' " + "/>");
+           // console.log(value.property2.src);
+
+           // $("#team1flag").append("<img src= " + " ' " + value.property2.src + " ' " + "/>");
+
+           $("#team2flag").append("<img src= " + " ' " + value.property2.src + " ' " + "/>");
 
             //  $("#team2flag").append("<p>" + value.property2.alt + "</p>");
           //  $("#flags").append("<option value='" + value.property2.alt + "'>" + "<img src= " + " ' " + value.property2.src + " ' " + "/></option>");
 
-            var selects = document.getElementById("team1");
-            var selectedValue = selects.options[selects.selectedIndex].value;// will gives u 2
-            if (selectedValue==16){
-                $("#team1flag").append("<img src= " + " ' " + value.property2.src[16] + " ' " + "/>");
-                alert('aloha');
+          //  var selects = document.getElementById("team1");
+            //var selectedValue = selects.options[selects.selectedIndex].value;// will gives u 2
+           // if (selectedValue==16){
+              //  alert('aloha');
+            $("#team1flag").append("<img src= " + " ' " + value.property2.src + " ' " + "/>");
 
-            }
+             // }
 
         }
+
+
+
     );
+
+
 }
 
 $.ajax({
